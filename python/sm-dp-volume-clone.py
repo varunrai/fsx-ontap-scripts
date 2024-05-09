96d5b98436232f2374af3b695323638a1a5f3c53
Varun Seth <1327311+varunrai@users.noreply.github.com>
2024-05-10 07:46:30 +0800

    fix pylint issues

--- python/sm-dp-volume-clone.py
+++ python/sm-dp-volume-clone.py
@@ -2,39 +2,40 @@ from netapp_ontap import config, HostConnection, NetAppRestError
 from netapp_ontap.resources import SnapmirrorRelationship, Volume
 import time
 
-fsxn_management_endpoint = "<FILESYSTEM MANAGMENT ENDPOINT>"
-fsxn_user = "fsxadmin"
-fsxn_user_pwd = "<PASSWORD>"
+FSXN_MANAGEMENT_ENDPOINT = "<FILESYSTEM MANAGMENT ENDPOINT>"
+FSXN_USER = "fsxadmin"
+FSXN_USER_PWD = "<PASSWORD>"
 
-svm_name = "fsx0svm1"
-vol_name = "vol_secondary"
-clone_name = "vol_clone"
-clone_junction_path = "/vol_clone"
+SVM_NAME = "fsx0svm1"
+VOL_NAME = "vol_secondary"
+CLONE_NAME = "vol_clone"
+CLONE_JUNCTION_PATH = "/vol_clone"
 
-config.CONNECTION = HostConnection(fsxn_management_endpoint, fsxn_user, fsxn_user_pwd, verify=False)
+config.CONNECTION = HostConnection(FSXN_MANAGEMENT_ENDPOINT, FSXN_USER, FSXN_USER_PWD, verify=False)
 
+""" Prints SnapMirror Relationship Info """
 def print_snapmirror_details(snapmirror):
     print("Source: " + snapmirror.source.path + " --> Destination: " +
           snapmirror.destination.path + ", UUID: " + snapmirror.uuid + ", Status: " + snapmirror.state)
 
-
+""" Updates SnapMirror Relationship State """
 def update_snapmirror_state(snapmirror, new_state):
     snapmirror.state = new_state
     if snapmirror.patch(poll=True):
         print("Snapmirror Relationship Updated Successfully to " + new_state)
 
-
+""" Handles NetApp REST exceptions """
 def handle_netapp_error(action, action_name, *args):
     try:
         return action(*args)
     except NetAppRestError as error:
         print("Exception caught while " + action_name + ": " + str(error))
 
-
+""" Find the SnapMirror Relationship for the Destination SVM and Volume name """
 def search_snapmirror_relationships():
-    return SnapmirrorRelationship.find(destination={"path": svm_name + ":" + vol_name})
+    return SnapmirrorRelationship.find(destination={"path": SVM_NAME + ":" + VOL_NAME})
 
-
+""" Delete Volume Clones """
 def delete_volume_clones(vol_name):
     for volume in Volume.get_collection(**{"clone.is_flexclone": True, "clone.parent_volume.name": vol_name}):
         print("Parent Volume: " + vol_name + " --> Clone: " +
@@ -42,7 +43,7 @@ def delete_volume_clones(vol_name):
         print("Deleting Clone: " + volume.name)
         volume.delete(force=True)
 
-
+""" Create Volume Clone """
 def create_clone(svm_uuid, vol_name, clone_name):
     print("Retrieving Parent Volume Details")
     parent_volume = Volume.find(name=vol_name)
@@ -52,20 +53,20 @@ def create_clone(svm_uuid, vol_name, clone_name):
     dataobj['svm'] = tmp
     dataobj['name'] = clone_name
     clone_volume_json = {"is_flexclone": bool("true"), "parent_svm": {
-        "name": svm_name, "uuid": svm_uuid}, "parent_volume": {"name": parent_volume.name, "uuid": parent_volume.uuid}}
+        "name": SVM_NAME, "uuid": svm_uuid}, "parent_volume": {"name": parent_volume.name, "uuid": parent_volume.uuid}}
     dataobj['clone'] = clone_volume_json
     volume = Volume.from_dict(dataobj)
     volume.post(poll=True)
 
 
 print("Searching for SnapMirror Relationship for Destination SVM: " +
-      svm_name + " and Volume: " + vol_name)
-snapmirror = handle_netapp_error(search_snapmirror_relationships, "searching for SnapMirror Relationships")
+      SVM_NAME + " and Volume: " + VOL_NAME)
+global snapmirror = handle_netapp_error(search_snapmirror_relationships, "searching for SnapMirror Relationships")
 print_snapmirror_details(snapmirror)
 
-svm_uuid = snapmirror.destination.svm.uuid
+global svm_uuid = snapmirror.destination.svm.uuid
 print("Searching for Clones of volume: " + snapmirror.destination.path)
-handle_netapp_error(delete_volume_clones, "searching for clones", vol_name)
+handle_netapp_error(delete_volume_clones, "searching for clones", VOL_NAME)
 
 
 print("Resuming the SnapMirror Relationship")
@@ -87,4 +88,4 @@ if snapmirror.state == "snapmirrored":
     update_snapmirror_state(snapmirror, "broken_off")
 
 handle_netapp_error(
-    create_clone, "retrieving parent volume details", svm_uuid, vol_name, clone_name)
+    create_clone, "retrieving parent volume details", svm_uuid, VOL_NAME, CLONE_NAME)
